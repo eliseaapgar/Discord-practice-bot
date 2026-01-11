17d2ef8e7a5d228d9742bea9c024ea4c198dcce0
Elisea <elisea.apgar@gmail.com>
2026-01-10 20:36:45 -0500

    Fixed intents configuration to enable message content

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import discord
 from discord.ext import commands
 import config 
 
-# Create bot instance
-bot = commands.Bot(command_prefix=config.COMMAND_PREFIX, intents=discord.Intents.default())
+# Create bot instance with intents
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=config.COMMAND_PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 # Bot configuration
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Bot settings
 COMMAND_PREFIX = '!'
